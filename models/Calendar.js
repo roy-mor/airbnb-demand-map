0e8891f771e89dcee05f67c8cce192da4879fc5a
Roy -- <roy@cherre.com>
2017-11-07 01:33:06 +0200

    added required fields for mongoose schema

--- models/Calendar.js
+++ models/Calendar.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 let Calendar = new mongoose.Schema({
     listing_id: {type: Number, index: true},
-    calendar_days: [Object]
-}); // no need to define specific fields
+    calendar_days: [Object],
+    occupancyScore: Number
+}, {strict: false}); // no need to define specific fields
 
 export default mongoose.model('Calendar', Calendar);
--- models/RawListing.js
+++ models/RawListing.js
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
 
 let RawListing = new mongoose.Schema({
+    pricing_quote: {
+        nightly_price: Number // TODO remove?
+    },
+    'airbnb-demand-location': {type: String, index: true},
     listing: { 
         id: {type: Number, index: true },
-        city: {type: String, index: true}
+        star_rating: Number,
+        reviews_count: Number,
+        nightly_price: Number,
+        primary_host: {
+            is_superhost: Boolean,
+        }
     }
-}); // no need to define specific fields
+}, {strict: false}); // no need to define specific fields
 
 export default mongoose.model('RawListing', RawListing);
