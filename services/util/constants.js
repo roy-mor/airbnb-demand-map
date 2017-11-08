a30a5dcc83ef6a6624aadf625ac8474b2a66f48d
Roy -- <roy@cherre.com>
2017-11-08 16:54:06 +0200

    reduced limit consts to more sane values

--- services/util/constants.js
+++ services/util/constants.js
@@ -11,8 +11,8 @@ const configurationConstants = {
 	 CLIENT_ID: 'd306zoyjsyarp7ifhu67rjxn52tv0t20', //'3092nxybyb0otqw18e8nh5nty';
 	 DEFAULT_LOCATION: 'Jerusalem',
 	 DEFAULT_LIMIT: 50,
-	 MAXIMUM_MAX_PRICE: 10000,
-	 
+	 MAXIMUM_MAX_PRICE: 5000, //$5000 per night is a reasonable price limit in most cities, for statistics purposes 
+ 	 MAXIMUM_LISTINGS_SEARCH_LIMIT: 10000, //10,000 listings is a reasonable cap that balances performance time and sample size for stats
 	 //to document: started with DEFAULT_PRICE_ADD: 5, but some busy cities had more than 1000 in that $5 range,
 	 //so change to 1, instead of implementing detection and fallback (more calls and a bit slower, but keeps it simple)
 	 //theoretically there could be a price tag with more than 1000 listings, but for our stats
