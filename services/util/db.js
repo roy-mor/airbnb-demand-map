cd14adeb31b153b350b733f30805f46b4ee754e3
Roy -- <roy@cherre.com>
2017-11-08 16:54:29 +0200

    added friendly error listener

--- services/util/db.js
+++ services/util/db.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import {mongodb} from 'c0nfig';
+//todo import logger?
 
 let db = mongoose.connection;
 
@@ -7,3 +8,8 @@ mongoose.connect(mongodb.defaultDB.connection, {
     user: mongodb.user,
     pass: mongodb.pass
 });
+
+db.on('error', err => console.error(`Failed to connect to database server ${mongodb.connection}. This code uses a mongo database; 
+it can be installed on localhost or remotely. See /config/*.config.js for db configuration.\n`, err));
+
+    
