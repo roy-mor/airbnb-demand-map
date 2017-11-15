6535077fbdc8b87000b2cd7bc7e18805229c93a3
Roy -- <roy@cherre.com>
2017-11-15 21:47:41 +0200

    beautify js indents

--- models/Calendar.js
+++ models/Calendar.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 
 let Calendar = new mongoose.Schema({
-    listing_id: {type: Number, index: true},
-    calendar_days: mongoose.Schema.Types.Mixed, 
+    listing_id: {
+        type: Number,
+        index: true
+    },
+    calendar_days: mongoose.Schema.Types.Mixed,
     occupancyScore: Number
-}, {strict: false}); // no need to indicate all specific fields
+}, {
+    strict: false
+}); // no need to indicate all specific fields
 
 export default mongoose.model('Calendar', Calendar);
--- models/RawListing.js
+++ models/RawListing.js
@@ -2,18 +2,29 @@ import mongoose from 'mongoose';
 
 let RawListing = new mongoose.Schema({
     pricing_quote: {
-        nightly_price: Number // TODO remove?
+        nightly_price: Number 
     },
-    'airbnb-demand-location': {type: String, index: true},
-    listing: { 
-        id: {type: Number, index: true },
-        star_rating: {type: Number, index: true},
+    'airbnb-demand-location': {
+        type: String,
+        index: true
+    },
+    listing: {
+        id: {
+            type: Number,
+            index: true
+        },
+        star_rating: {
+            type: Number,
+            index: true
+        },
         reviews_count: Number,
         nightly_price: Number,
         primary_host: {
             is_superhost: Boolean,
         }
     }
-}, {strict: false}); // no need to indicate all specific fields
+}, {
+    strict: false
+}); // no need to indicate all specific fields
 
 export default mongoose.model('RawListing', RawListing);
