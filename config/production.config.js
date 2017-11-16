5c2fa823e680a9d9e6bc7340973c16a38b11e621
Roy -- <roy@cherre.com>
2017-11-16 03:47:28 +0200

    added production config file for heroku

---
+++ config/production.config.js
@@ -0,0 +1,20 @@
+const LOCALHOST = '127.0.0.1';
+module.exports = {
+    port: process.env.PORT || process.env.NODE_PORT || 8081,
+    ssl: false,
+    host: LOCALHOST,
+    serverIp: LOCALHOST,
+    mongodb: {
+        host: LOCALHOST,
+        port: '27017',
+        user: process.env.MONGODB_USER || '',
+        pass: process.env.MONGODB_PASS || '',
+        defaultDB: {
+            user: '$(mongodb.user)',
+            pass: '$(mongodb.pass)',
+            name: 'airbnb-demand',
+            connection: process.env.UD_MONGODB_CONNECTION || 'mongodb://$(mongodb.host):$(mongodb.port)/$(mongodb.defaultDB.name)'
+       }
+   }
+};
+
