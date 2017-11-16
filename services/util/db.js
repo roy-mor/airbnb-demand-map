c4be28a7676879e129a321576b810699a4ca4c84
Roy -- <roy@cherre.com>
2017-11-16 20:37:11 +0200

    fixed timeout issues

--- services/util/db.js
+++ services/util/db.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
-import {mongodb} from 'c0nfig';
+import { mongodb } from 'c0nfig';
 mongoose.Promise = require('bluebird');
 
 let db = mongoose.connection;
 
 mongoose.connect(mongodb.defaultDB.connection, {
-    user: mongodb.user,
-    pass: mongodb.pass
+	user: mongodb.user,
+	pass: mongodb.pass,
+	useMongoClient: true,
+	reconnectTries: Number.MAX_VALUE,
+	server: {
+		keepAlive: 150,
+		connectTimeoutMS: 1000 * 600,
+		socketTimeoutMS: 1000 * 600,
+		reconnectInterval: 1000 * 10
+	}
 });
 
 db.on('error', err => console.error(`Failed to connect to database server ${mongodb.defaultDB.connection}. 
