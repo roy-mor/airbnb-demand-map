d5c05eee261aa29fd02d7710b775bed09d8e74b6
Roy -- <roy@cherre.com>
2017-11-18 17:54:18 +0200

    changed production db

--- config/production.config.js
+++ config/production.config.js
@@ -1,7 +1,7 @@
 // This config file will be read when NODE_ENV environment variable equals 'production'.
 // Update data below to your own production environment.
 
-const MLAB_HOST = 'ds113386-a0.mlab.com'
+const MLAB_HOST = 'ds227035.mlab.com';
 module.exports = {
     port: process.env.PORT || process.env.NODE_PORT || 8081,
     ssl: false,
@@ -9,13 +9,13 @@ module.exports = {
     serverIp: MLAB_HOST,
     mongodb: {
         host: MLAB_HOST,
-        port: '13386',
+        port: '27035',
         user: process.env.MONGODB_USER || 'airbnbdemanduser', // normally this should be in an environment var or config var
         pass: process.env.MONGODB_PASS || 'airbnbdemanduser', // normally this should be in an environment var or config var
         defaultDB: {
             user: '$(mongodb.user)',
             pass: '$(mongodb.pass)',
-            name: 'airbnb-demand-1',
+            name: 'airbnb-demand',
             connection: process.env.UD_MONGODB_CONNECTION || 'mongodb://$(mongodb.host):$(mongodb.port)/$(mongodb.defaultDB.name)'
        }
    }
