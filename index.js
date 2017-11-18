bc6b6da77170bec55e968a7f45e383197cfc0235
Roy -- <roy@cherre.com>
2017-11-18 05:40:03 +0200

    error handler

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message, 
+            error: err
+        });
+     });
+ }
+ 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
