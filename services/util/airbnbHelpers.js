6eede5409211287daf42b31d742745f7ce35bebe
Roy -- <roy@cherre.com>
2017-11-18 05:25:52 +0200

    deleted aggregate pipeline

--- services/util/airbnbHelpers.js
+++ services/util/airbnbHelpers.js
@@ -20,35 +20,4 @@ function buildSearchUrl({clientId, location, offset, limit, priceMin, priceMax})
     return url;
 }
 
-
-/*function createDemandPipeline(location) {
-    const pipeline = [{
-            $match: {
-                'airbnb-demand-location': location,
-            }
-        },
-        {
-            $lookup: { //JOIN rawlistings and calendars based on airbnb listing_id
-                from: 'calendars',
-                localField: 'listing.id',
-                foreignField: 'listing_id',
-                as: 'calendar'
-            }
-        },
-        {
-            $unwind: '$calendar'
-        },
-        {
-            $match: {
-                // filter out all listings which are "always occupied" the entire year but have no star rating;
-                // in that case they are just inactive dummy listings blocked for the year, not real active listings,
-                // and are contaminating our stats
-                $or: [{'calendar.occupancyScore': { $ne: consts.FULL_CALENDAR_DAYS }},
-                     { 'listing.star_rating': { $ne: null } } ]
-            }
-        }
-    ];
-    return pipeline;
-}
-*/
 module.exports = { buildSearchUrl, buildCalendarUrl };
